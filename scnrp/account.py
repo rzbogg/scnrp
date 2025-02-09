51431adc47cc168ed09a33d54c7c9ad654fd4463
Reza Bolhasani <rzbo.35@gmail.com>
2025-02-09 22:29:08 +0330

    refactor: better data retriving

--- scnrp/account.py
+++ scnrp/account.py
@@ -1,26 +1,33 @@
 from xrpl.models.requests import AccountInfo,AccountTx
+from xrpl.models.response import Response, ResponseStatus
 from api import APIClient
 
 class Account:
     def __init__(
         self,
         address,
-        balance: int = 0,
+        data ,
     ) -> None:
         self.address = address
-        self.balance = balance
+        self._data = data
 
     @classmethod
     def from_json(cls,json):
         return cls(
             address= json['account_data']['Account'],
-            balance= json['account_data']['Balance']
+            data = json['account_data']
         )
 
     def __repr__(self) -> str:
-        return f'Account({self.address},{self.balance})'
+        return f'Account({self.address})'
+
+    def fields(self):
+        return self._data.items()
+
 
 def get_account(account:str):
     req_account = AccountInfo(account=account)
     res_account = APIClient.request(req_account)
+    if res_account.status == ResponseStatus.ERROR:
+        raise ValueError('unable to get account')
     return Account.from_json(res_account.result)
