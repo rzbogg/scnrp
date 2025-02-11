ec2bd9f46e168ff9181078f81bb2b8f298efba9d
Reza Bolhasani <rzbo.35@gmail.com>
2025-02-11 18:58:31 +0330

    refactor

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from scnrp.command import create_command
+from scnrp.cli import arg_parser
 import sys
 
 
 def show_help():
     print('usage: scnrp command parameters...')
 
-if len(sys.argv) < 2:
-    show_help()
-else:
-    command = create_command(sys.argv[1:])
+def main():
+    args = arg_parser.parse_args()
+    command = create_command(args)
     print(command.run())
+
+main()
