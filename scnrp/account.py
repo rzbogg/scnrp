743ef1d9763a10d6134cf08416ee8f9368204c29
Reza Bolhasani <rzbo.35@gmail.com>
2025-02-19 21:27:30 +0330

    add: some additional data

--- scnrp/account.py
+++ scnrp/account.py
@@ -1,5 +1,7 @@
 from xrpl.models.requests import AccountInfo
 from xrpl.models.response import ResponseStatus
+from xrpl.utils import drops_to_xrp
+
 from . import api
 from .transaction import get_account_txs
 
@@ -9,12 +11,22 @@ class AccountData:
         address:str,
         last_tx:str,
         balance:int,
+        next_seq:int,
         txs = [],
     ) -> None:
         self.address = address
         self.last_tx = last_tx
-        self.balance = balance
+        self._balance = balance
+        self.next_seq = next_seq
         self.txs = txs
+
+    @property
+    def balance(self):
+        return drops_to_xrp(str(self._balance))
+
+    @property
+    def activated_by(self):
+        return self.txs[-1].account
 
     def _txs_summary(self):
         result = '\n---------\n'
@@ -28,8 +40,8 @@ class AccountData:
         return f'''Address: {self.address}
 Balance: {self.balance}
 Last transaction: {self.last_tx}
-{self._txs_summary()}
-'''
+Next Sequence: {self.next_seq}
+Activated By: {self.activated_by}'''
     def flag_summary(self,address,balance,last_tx,txs):
         result = ''
         if address:
@@ -37,7 +49,7 @@ Last transaction: {self.last_tx}
         if balance:
             result += f'Balance: {self.balance}\n'
         if last_tx:
-            result += f'Last Transaction: {self.last_tx}\n'
+            result += f'{self.txs[0].summary()}\n'
         if txs:
             result += f'Transactions:{self._txs_summary()}'
         assert result != ''
@@ -52,6 +64,7 @@ Last transaction: {self.last_tx}
             address = json['Account'],
             balance = json['Balance'],
             last_tx = json['PreviousTxnID'],
+            next_seq = json['Sequence']
         )
 
 
@@ -63,5 +76,4 @@ def get_account(account:str):
     account_data = AccountData.from_json(res_account.result['account_data'])
     for tx in get_account_txs(account):
         account_data.add_tx(tx)
-
     return account_data
