7de69f1f5967211f9d74acd48c1278eaec8ac798
Reza Bolhasani <rzbo.35@gmail.com>
2025-02-26 20:07:20 +0330

    bug fixes in amount method

--- scnrp/transaction/payment.py
+++ scnrp/transaction/payment.py
@@ -6,14 +6,13 @@ class PaymentTxData:
         self.base = base_data
         self._amount = amount
         self.destination = destination
+
     @property
     def amount(self):
-        if isinstance(self._amount,int):
-            return drops_to_xrp(str(self._amount))
-        elif isinstance(self._amount,dict):
-            return f"{drops_to_xrp(str(self._amount['value']))}' {self._amount['currency']}"
-        else:
-            return drops_to_xrp(self._amount)
+        if isinstance(self._amount,dict):
+            return f"{str(self._amount['value'])}' {self._amount['currency']}"
+        return f'{drops_to_xrp(str(self._amount))} XRP'
+
     def summary(self):
         return f'''[Payment][{self.base.status}]
 Transaction Hash: {self.base.hash}
